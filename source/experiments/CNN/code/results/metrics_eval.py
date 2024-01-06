25b36c2e8734e8af1832fab9960624a657a2d61d
bastis007 <schaefer.bastian@mail.de>
2024-01-06 16:45:00 +0100

    CNN - Update metrics evaluation

--- source/experiments/CNN/code/results/metrics_eval.py
+++ source/experiments/CNN/code/results/metrics_eval.py
@@ -2,6 +2,8 @@
 In this file, we want to read the predictions from the predictions file of the model
 and use the metrics to evaluate the quality.
 """
+import os
+
 from torchmetrics.text import TranslationEditRate
 from torchmetrics.text.bert import BERTScore
 from torchmetrics.text.bleu import BLEUScore
@@ -33,32 +35,38 @@ def use_metrics(predicted_text, ground_truth_text):
     wer = WordErrorRate()
     wer_score = wer(predicted_text, ground_truth_text)
     results.append("WER: " + str(wer_score.item()))
-    # BERT
-    #bert = BERTScore()
-    #if not predicted_text:
-        #bert_score = "None"
-    #else:
-        #bert_score = bert(predicted_text, ground_truth_text)
-    #results.append("BERT: " + str(bert_score))
-    #return results
+    # BERT (not working, as the length of the predicted and the original text are not of the same length)
+    bert = BERTScore()
+    if not predicted_text or not len(predicted_text) == len(ground_truth_text):
+        bert_score = "None"
+    else:
+        bert_score = bert(predicted_text, ground_truth_text)
+    results.append("BERT: " + str(bert_score))
+    return results
 
+def create_metric_file(method):
+    path = './evaluation/eval_metrics_' + method + '.txt'
+    if not os.path.exists(path):
+        with open(path, 'w'): pass
 
 def write_metric_results(results, method):
-    print(method)
-    with open(f'./evaluation/eval_metrics_{method}.txt', 'a', encoding='utf-8') as file:
-        file.write("Evaluation Metrics for " + str(method) + "\n" + str(results))
+    with open(f'./evaluation/eval_metrics_{method}.txt', 'w', encoding='utf-8') as file:
+        for result in results:
+            file.write(str(result) + "\n")
 
 
 for i in range(len(method_name)):
     with open(f'./predictions/model_predictions_{method_name[i]}.txt', 'r', encoding='utf-8') as file:
         lines = file.readlines()
+        create_metric_file(method_name[i])
+        metric_results = [method_name[i]]
         for line in lines:
             if "Predicted (French): " in line:
                 prediction = line.split("Predicted (French): ")[1]
             elif "Ground Truth (French): " in line:
                 ground_truth = line.split("Ground Truth (French): ")[1]
             elif "----------" in line:
-                metric_results = use_metrics(prediction, ground_truth)
-                print(metric_results)
-                write_metric_results(metric_results, method_name[i])
-                print("Results written")
+                metric_result = use_metrics(prediction, ground_truth)
+                metric_results.append(str(metric_result))
+        write_metric_results(metric_results, method_name[i])
+        print("All metrics are calculated for " + method_name[i])
