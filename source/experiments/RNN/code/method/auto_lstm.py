af462e671470692c76945f4bb8a675460f714473
fcortesj <131784473+fcortesj98@users.noreply.github.com>
2024-01-12 14:38:20 +0100

    feature/adding_regularization_auto_rnn
    adding regularization in auto encoders

--- source/experiments/RNN/code/method/auto_lstm.py
+++ source/experiments/RNN/code/method/auto_lstm.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Embedding, LSTM, Dense
+from tensorflow.keras.layers import Input, Embedding, LSTM, Dense, Dropout
 
 class EncoderDecoderLSTMModel:
     def __init__(self, tokenizer_en, tokenizer_fr, max_vocab_fr_len):
@@ -14,8 +14,10 @@ class EncoderDecoderLSTMModel:
 
         # Adding four LSTM layers in the encoder
         encoder_lstm1 = LSTM(32, return_sequences=True)(enc_emb)
+        encoder_dropout1 = Dropout(0.5)(encoder_lstm1)
         encoder_lstm2 = LSTM(32, return_sequences=True)(encoder_lstm1)
-        _, state_h, state_c = LSTM(32, return_state=True)(encoder_lstm2)
+        encoder_dropout2 = Dropout(0.5)(encoder_lstm2)
+        _, state_h, state_c = LSTM(32, return_state=True)(encoder_dropout2)
         encoder_states = [state_h, state_c]
 
         # Decoder
@@ -25,8 +27,10 @@ class EncoderDecoderLSTMModel:
 
         # Adding four LSTM layers in the decoder
         decoder_lstm1 = LSTM(32, return_sequences=True, return_state=True)(dec_emb, initial_state=encoder_states)
-        decoder_lstm2 = LSTM(32, return_sequences=True)(decoder_lstm1[0])
-        decoder_lstm3 = LSTM(16, return_sequences=True)(decoder_lstm2)
+        decoder_dropout1 = Dropout(0.5)(decoder_lstm1[0])  # Dropout after the first LSTM layer
+        decoder_lstm2 = LSTM(32, return_sequences=True)(decoder_dropout1)
+        decoder_dropout2 = Dropout(0.5)(decoder_lstm2)  # Another dropout layer
+        decoder_lstm3 = LSTM(16, return_sequences=True)(decoder_dropout2)
 
         decoder_dense = Dense(self.max_vocab_fr_len + 1, activation='softmax')
         decoder_outputs = decoder_dense(decoder_lstm3)
